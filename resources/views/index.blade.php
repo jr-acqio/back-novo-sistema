98bdc9a136497283b85fd7e653632f54208056b5
Junior Paiva <juniorpaiva95@gmail.com>
2017-05-19 23:34:10 -0300

    Atualização do cliente

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,4 +3,4 @@
       js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
       js.src='https://apis.google.com/js/platform.js';
       fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
-    }(window,document,'script'));</script><link href=/static/css/app.a567c0a07c590dfcfd107190d2027ba3.css rel=stylesheet></head><body><div id=app class=""><app></app></div><script type=text/javascript src=/static/js/manifest.cbf83ccb35f06d00ac8e.js></script><script type=text/javascript src=/static/js/vendor.7207f7b4513034fda999.js></script><script type=text/javascript src=/static/js/app.cf3ce3183e14278b1d62.js></script></body></html>
+    }(window,document,'script'));</script><link href=/static/css/app.a567c0a07c590dfcfd107190d2027ba3.css rel=stylesheet></head><body><div id=app class=""><app></app></div><script type=text/javascript src=/static/js/manifest.ef06dc4caf167f679c9c.js></script><script type=text/javascript src=/static/js/vendor.7207f7b4513034fda999.js></script><script type=text/javascript src=/static/js/app.c31634dcd5ab4a0e3099.js></script></body></html>
