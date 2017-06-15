3b2a53680ce8b822330df87c1c932b22c9140607
Lucas Dantas <luk.dantas@gmail.com>
2017-06-15 12:52:18 -0300

    Criacao de Permissoes.

--- Modules/Access/Contracts/PermissionRepository.php
+++ Modules/Access/Contracts/PermissionRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Access\Access\Contracts;
+namespace Modules\Access\Contracts;
 
 use Prettus\Repository\Contracts\RepositoryInterface;
 
--- Modules/Access/Entities/Permission.php
+++ Modules/Access/Entities/Permission.php
@@ -2,9 +2,12 @@
 
 namespace Modules\Access\Entities;
 
+use OwenIt\Auditing\Auditable;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Zizaco\Entrust\EntrustPermission;
 
-class Permission extends EntrustPermission
+class Permission extends EntrustPermission implements AuditableContract
 {
+    use Auditable;
     protected $fillable = [];
 }
